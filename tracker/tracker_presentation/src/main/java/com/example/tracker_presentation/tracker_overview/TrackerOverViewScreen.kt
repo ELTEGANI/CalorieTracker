9cf6057df76397c68c0a17faa572730dc4035925
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-03-14 17:02:01 +0400

    expandable meal ui

--- tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -2,12 +2,14 @@ package com.example.tracker_presentation.tracker_overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.core.util.UiEvent
 import com.example.core_ui.LocalSpacing
 import com.example.tracker_presentation.tracker_overview.components.DaySelector
+import com.example.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.example.tracker_presentation.tracker_overview.components.NutrientHeader
 
 
@@ -36,10 +38,18 @@ fun TrackerOverViewScreen(
                viewModel.onEvent(TrackerOverViewEvent.OnNextDayClick)
               },
               modifier = Modifier
-              .fillMaxSize()
-              .padding(horizontal = spacing.spaceMedium)
+                  .fillMaxSize()
+                  .padding(horizontal = spacing.spaceMedium)
           )
           Spacer(modifier = Modifier.height(spacing.spaceMedium))
+      }
+      items(state.meals){meal->
+          ExpandableMeal(
+              meal = meal,
+              onToggleMealClick = {viewModel.onEvent(TrackerOverViewEvent.OnToggleMealClick(meal))},
+              content = { /*TODO*/ },
+              modifier = Modifier.fillMaxWidth()
+          )
       }
   }
 }
